a7ef6669f7932979b7658691d3e16b1754983281
Jaskirat Singh <38223354+JaskiratSingh1@users.noreply.github.com>
2024-06-11 17:37:20 -0600

    init and dataframe setup complete

--- Assignments/assignment4/calgary_dogs.py
+++ Assignments/assignment4/calgary_dogs.py
@@ -1,5 +1,5 @@
 # calgary_dogs.py
-# AUTHOR NAME
+# Jaskirat Singh
 #
 # A terminal-based application for computing and printing statistics based on given input.
 # Detailed specifications are provided via the Assignment 4 README file.
@@ -7,15 +7,39 @@
 # You may import any modules from the standard Python library.
 # Remember to include docstrings and comments.
 
-def main():
+import pandas as pd
+import os
 
+def main():
     # Import data here
+    dog_breeds_data = pd.read_excel("CalgaryDogBreeds.xlsx")
 
     print("ENSF 692 Dogs of Calgary")
 
-    # User input stage
+    # User input stage and convert to all uppercase to make it case-insensitive
+
+    while True:
+        dog_breed_input = input("Please enter a dog breed: ").upper()
+        try:
+            if dog_breed_input in dog_breeds_data["Breed"].values:
+                break
+            else: raise KeyError
+        except KeyError:
+            print("Dog breed not found in the data. Please try again.")
 
     # Data anaylsis stage
+    # Get rows with input breed as the breed
+    dog_breed_data = dog_breeds_data[dog_breeds_data["Breed"].str.upper() == dog_breed_input]
+    # Get list of unique years of data entries (no need to sort as data is pre-sorted)
+    years_listed = dog_breed_data["Year"].unique()
+
+    # Print message for project requirement
+    print(f"The {dog_breed_input} was found in the top breeds for years: {' '.join(map(str, years_listed))}")
+
+    
+
+
+
 
 if __name__ == '__main__':
     main()
